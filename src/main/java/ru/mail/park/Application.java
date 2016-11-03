c557c2ba9a5efbe956df5642a112526670635174
Anatoliuz <afilinovs@gmail.com>
2016-11-03 14:49:44 +0300

    make it better

--- src/main/java/ru/mail/park/Application.java
+++ src/main/java/ru/mail/park/Application.java
@@ -3,10 +3,6 @@ package ru.mail.park;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by Zac on 01/10/16.
- */
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
